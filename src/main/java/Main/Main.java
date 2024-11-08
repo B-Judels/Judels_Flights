38b56c2c5e315c35f26e630339f61b5893e8c8ac
Bernard <judelsbernard2@gmail.com>
2024-11-08 12:08:57 +0200

    some changes to login screen

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,8 +1,20 @@
 package Main;
 
 
+import Screens.LoginScreen;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                LoginScreen main = new LoginScreen();
+                main.show();
+            }
+        });
 
     }
 }
--- src/main/java/Screens/LoginScreen.java
+++ src/main/java/Screens/LoginScreen.java
@@ -1,4 +1,64 @@
 package Screens;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.jar.JarEntry;
+
 public class LoginScreen {
+
+    private JFrame frame;
+    private JPanel panel;
+    private JTextField username;
+    private JPasswordField password;
+    private JButton login;
+    private JLabel label;
+
+    public LoginScreen(){
+        frame = new JFrame("Login Screen");
+        frame.setSize(500, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        panel = new JPanel();
+        panel.setLayout(null);
+        frame.add(panel);
+
+        label = new JLabel("Welcome to Judels Flights!");
+        label.setBounds(100, 10, 300, 30);
+        label.setFont(new Font("Tahoma", Font.BOLD, 20));
+        panel.add(label);
+
+        label = new JLabel("Login");
+        label.setBounds(210, 50, 50, 30);
+        label.setFont(new Font("Tahoma", Font.BOLD, 18));
+        panel.add(label);
+
+        // username input
+        label = new JLabel("Username:");
+        label.setBounds(150, 82, 100, 20);
+        label.setFont(new Font("Tahoma", Font.BOLD, 12));
+        panel.add(label);
+
+        username = new JTextField();
+        username.setBounds(150, 100, 200, 30);
+        panel.add(username);
+
+        //password input
+        label = new JLabel("Password:");
+        label.setBounds(150, 132, 100, 20);
+        label.setFont(new Font("Tahoma", Font.BOLD, 12));
+        panel.add(label);
+
+        password = new JPasswordField();
+        password.setBounds(150, 150, 200, 30);
+        panel.add(password);
+
+        login = new JButton("Login");
+        login.setBounds(200, 200, 100, 30);
+        panel.add(login);
+    }
+    public void show(){
+        frame.setVisible(true);
+    }
 }
